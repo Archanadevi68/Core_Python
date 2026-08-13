c401595c714c3fd8b0be0ad0169b68c8f9ab237c
Archana Devi <archanadevi6824@gmail.com>
2026-08-13 17:25:59 +0530

    c1

--- banking.py
+++ banking.py
@@ -1,7 +1,11 @@
-a = int(input("Enter an integer: "))
-b = int(input("Enter another integer: "))
-def add(a,b):
-    x=a+b
-    return x
-print(add(a,b))
-print(add(a,b))
+# a = int(input("Enter an integer: "))
+# b = int(input("Enter another integer: "))
+# def add(a,b):
+#     x=a+b
+#     return x
+# print(add(a,b))
+# print(add(a,b))
+#
+
+
+print("hello archanan")
